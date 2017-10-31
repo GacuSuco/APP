73f655c84aebdfafab6177bdbaabcbbe0df01d46
[Desktop]: Mitch Peters <27728981+GacuSuco@users.noreply.github.com>
2017-10-31 22:37:29 +0100

    AST lvl 2

--- src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -11,7 +11,7 @@ public class ASTListener extends ICSSBaseListener {
 	
 	//Accumulator attributes:
 	private AST ast;
-	private Stack<ASTNode> currentContainer; //Yes, this is a hint...
+	private Stack<ASTNode> currentContainer; //Yes, this is a hint... OK
 
 	public ASTListener() {
 		ast = new AST();
@@ -74,37 +74,23 @@ public class ASTListener extends ICSSBaseListener {
 
 	@Override
 	public void enterColor(ICSSParser.ColorContext ctx) {
-		currentContainer.peek().addChild(new ColorLiteral(ctx.getText()));
+		currentContainer.push(new ColorLiteral(ctx.getText()));
 	}
 	@Override
 	public void enterPixelLiteral(ICSSParser.PixelLiteralContext ctx) {
-		currentContainer.peek().addChild(new PixelLiteral(ctx.getText()));
+		currentContainer.push(new PixelLiteral(ctx.getText()));
 	}
 	@Override
 	public void enterPercentageLiteral(ICSSParser.PercentageLiteralContext ctx) {
-		currentContainer.peek().addChild(new PercentageLiteral(ctx.getText()));
+		currentContainer.push(new PercentageLiteral(ctx.getText()));
 	}
-
-	@Override
-	public void enterVariableDeclaration(ICSSParser.VariableDeclarationContext ctx) {
-		currentContainer.push(new Declaration());
-	}
-
-	@Override
-	public void exitVariableDeclaration(ICSSParser.VariableDeclarationContext ctx) {
-		Declaration declaration = ((Declaration) currentContainer.pop());
-		currentContainer.peek().addChild(declaration);
-	}
-
-	// Shit omgooien !!! idee nu is dat variable, color, pixal, en perc eerst worden toegevoegt. daarnaa wordt er gekeken of operation nodig is.
-
 	@Override
 	public void enterVariable(ICSSParser.VariableContext ctx) {
-		if (((Declaration) currentContainer.peek()).property == null){
+		if (ctx.parent.getChildCount() != 1){ //maakt deel uit van de tree
 			((Declaration) currentContainer.peek()).property = ctx.getText();
 		}
 		else {
-			currentContainer.peek().addChild(new VariableReference(ctx.getText()));
+			currentContainer.push(new VariableReference(ctx.getText()));
 		}
 	}
 
@@ -112,25 +98,41 @@ public class ASTListener extends ICSSBaseListener {
 	public void enterOperator(ICSSParser.OperatorContext ctx) {
 		currentContainer.add(new Operation(Operation.Operator.fromString(ctx.getText())));
 	}
-
-
 	@Override
-	public void enterExpression(ICSSParser.ExpressionContext ctx) {
-		System.out.println(ctx.getChildCount());
-		if (ctx.getChildCount() > 1) { // wanneer count > is dan 1 heeft een operator
-			for (int i = 0; i < ctx.getChildCount(); i++) {
-				if (i% 2 != 0){
-					System.out.println("\ni'm an operator");
-					System.out.println(ctx.getChild(i).getText());
+	public void enterVariableDeclaration(ICSSParser.VariableDeclarationContext ctx) {
+		currentContainer.push(new Declaration());
+	}
+	@Override
+	public void exitVariableDeclaration(ICSSParser.VariableDeclarationContext ctx) {
+		Declaration declaration = ((Declaration) currentContainer.pop());
+		currentContainer.peek().addChild(declaration);
+	}
+	@Override
+	public void exitExpression(ICSSParser.ExpressionContext ctx) {
+		// kijk naar het aantal children
+		// als een voeg direct to
+		// als meer dan 1 check voor operators op even getalen;
+		// pop die shit
+		if (ctx.getChildCount() > 1){
+
+			for (int i = 2; i < ctx.getChildCount(); i+=2) {
+				ASTNode right = this.currentContainer.pop();
+				ASTNode operator = this.currentContainer.pop();
+				ASTNode left = this.currentContainer.pop();
+
+				((Operation)operator).rhs = (Expression) right;
+				((Operation)operator).lhs = (Expression) left;
+
+				if (i + 2 < ctx.getChildCount()){
+					this.currentContainer.push(operator);
 				}else {
-					System.out.println("\ni'm an value\n" + ctx.getChild(i).getText());
+					this.currentContainer.peek().addChild(operator);
 				}
 			}
 		}
-
-
-		super.enterExpression(ctx);
+		else{
+			ASTNode node = this.currentContainer.pop();
+			this.currentContainer.peek().addChild(node);
+		}
 	}
-
-
 }
