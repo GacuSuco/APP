0d1cb2f882282794ce325541a0ccc446ecde8ee4
[Desktop]: Mitch Peters <27728981+GacuSuco@users.noreply.github.com>
2017-11-02 15:52:57 +0100

    Transformeren lvl 2 + comments

--- src/main/java/nl/han/ica/icss/transforms/EliminateOptionals.java
+++ src/main/java/nl/han/ica/icss/transforms/EliminateOptionals.java
@@ -5,5 +5,22 @@ import nl.han.ica.icss.ast.*;
 public class EliminateOptionals implements Transform {
     @Override
     public void apply(AST ast) {
+        // lijst met alle conditional stylerule die condiie false hebben
+        java.util.Stack<ASTNode> garbage = new java.util.Stack<>();
+
+        // zoek naar false conditional stylerules
+        for (ASTNode n: ast.root.getChildren()) {
+            if(n.getClass() == Stylerule.class){
+                if(((Stylerule) n).condition != null){
+                    if(!((BoolLiteral) ((Stylerule) n).condition).value){
+                        garbage.push(n);
+                    }
+                }
+            }
+        }
+
+        // delete alle childs uit root die in gabarge zitten.
+        garbage.forEach((node) ->  ast.root.removeChild(node));
     }
+
 }
